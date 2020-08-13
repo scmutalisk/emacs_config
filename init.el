7ecc70e436dc9cc96758be87fafb472260f8246c
scmutalisk <scmutalisk@protonmail.com>
2020-08-13 21:06:17 +0300

    another configuration

---
+++ init.el
@@ -0,0 +1,17 @@
+(require 'org)
+(require 'ob-tangle)
+(setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
+(org-babel-load-file (expand-file-name "readme.org" init-dir))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(elpy vue-mode beacon dired-narrow emmet-mode magit tramp-term try kivy-mode js2-mode elcord nim-mode multiple-cursors ido-vertical-mode smex zenburn-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
