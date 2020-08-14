c9c3610a25056c4bf6ab6d3f53f954b8610a7463
scmutalisk <scmutalisk@protonmail.com>
2020-08-14 20:15:56 +0300

    pdf tools

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(elpy vue-mode beacon dired-narrow emmet-mode magit tramp-term try kivy-mode js2-mode elcord nim-mode multiple-cursors ido-vertical-mode smex zenburn-theme use-package)))
+   '(pdf-tools elpy vue-mode beacon dired-narrow emmet-mode magit tramp-term try kivy-mode js2-mode elcord nim-mode multiple-cursors ido-vertical-mode smex zenburn-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
