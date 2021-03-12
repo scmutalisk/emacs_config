1acde99a1929016c8533e6255589d5702961e4dc
scmutalisk <scmutalisk@protonmail.com>
2021-03-12 10:04:40 +0200

    added glsl mode

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme vue-mode use-package try tramp-term smex pdf-tools nim-mode multiple-cursors magit kivy-mode js2-mode ido-vertical-mode emmet-mode elpy elcord dired-narrow company-web company-try-hard company-statistics company-quickhelp beacon)))
+   '(glsl-mode zenburn-theme vue-mode use-package try tramp-term smex pdf-tools nim-mode multiple-cursors magit kivy-mode js2-mode ido-vertical-mode emmet-mode elpy elcord dired-narrow company-web company-try-hard company-statistics company-quickhelp beacon)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
