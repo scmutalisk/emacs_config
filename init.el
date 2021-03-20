12840c7082cd98e392cebb38ce00aacb70afab80
scmutalisk <scmutalisk@protonmail.com>
2021-03-20 17:01:28 +0200

    removed unused packages

--- init.el
+++ init.el
@@ -7,6 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
  '(package-selected-packages
    '(glsl-mode zenburn-theme vue-mode use-package try tramp-term smex pdf-tools nim-mode multiple-cursors magit kivy-mode js2-mode ido-vertical-mode emmet-mode elpy elcord dired-narrow company-web company-try-hard company-statistics company-quickhelp beacon)))
 (custom-set-faces
